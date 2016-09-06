c51ec142f299f3aff6d93c448aa22d34e16aca5a
Nathan Fiscaletti <nathanf@Nathans-MacBook-Pro.local>
2016-09-06 10:08:00 -0700

    Fixed more Style CI Issues

--- src/Synful/Standalone/ClientHandleMultiThread.php
+++ src/Synful/Standalone/ClientHandleMultiThread.php
@@ -1,62 +1,61 @@
 <?php
-    
+
 namespace Synful\Standalone;
 
 use Synful\Synful;
-use Synful\Controller;
 use Synful\IO\IOFunctions;
 use Synful\IO\LogLevel;
 use Synful\Util\Object;
 
 /**
- * Class used for handling multithread client socket communication
+ * Class used for handling multithread client socket communication.
  */
 class ClientHandleMultiThread extends Thread
 {
     use Object;
 
     /**
-     * The socket used for the client connection
+     * The socket used for the client connection.
      *
      * @var socket
      */
     private $client_socket;
 
     /**
-     * The ip address of the connecting client
+     * The ip address of the connecting client.
      *
      * @var string
      */
     private $ip;
 
     /**
-     * The port of the connecting client
+     * The port of the connecting client.
      *
      * @var int
      */
     private $port;
 
     /**
-     * Initializes the new handler for the request
+     * Initializes the new handler for the request.
      */
     public function run()
     {
-            $input = socket_read($this->client_socket, 2024);
+        $input = socket_read($this->client_socket, 2024);
 
-            IOFunctions::out(
-                LogLevel::INFO,
-                'Client REQ ('.$this->ip.':'.$this->port.'): '.$input
-            );
+        IOFunctions::out(
+            LogLevel::INFO,
+            'Client REQ ('.$this->ip.':'.$this->port.'): '.$input
+        );
 
-            $response = Synful::$controller->handleRequest($input, $this->ip);
-            socket_write($this->client_socket, json_encode($response), strlen(json_encode($response)));
+        $response = Synful::$controller->handleRequest($input, $this->ip);
+        socket_write($this->client_socket, json_encode($response), strlen(json_encode($response)));
 
-            IOFunctions::out(
-                LogLevel::INFO,
-                'Server RES ('.$this->ip.':'.$this->port.'): '.json_encode($response)
-            );
+        IOFunctions::out(
+            LogLevel::INFO,
+            'Server RES ('.$this->ip.':'.$this->port.'): '.json_encode($response)
+        );
 
-            socket_close($this->client_socket);
-            unset($this);
+        socket_close($this->client_socket);
+        unset($this);
     }
 }
