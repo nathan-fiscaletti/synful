5d0a76d8d3a1ad4a90964071cb9fb4a0ab8f57fb
Nathan Fiscaletti <nathhan.fiscaletti@gmail.com>
2016-10-03 10:58:24 -0700

    Added pretty_responses config parameter and optional ?pretty get parameter

--- config/System.php
+++ config/System.php
@@ -65,4 +65,16 @@ return [
 
     'production' => true,
 
+
+    /*
+     |--------------------------------------------------------------------------
+     | Pretty Responses
+     |--------------------------------------------------------------------------
+     |
+     | If set to true, JSON responses will be formatted using JSON_PRETTY_PRINT.
+     | You can also use the ?pretty GET parameter.
+     */
+
+    'pretty_responses' => false,
+
 ];
