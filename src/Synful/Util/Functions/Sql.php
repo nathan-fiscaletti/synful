cdf88cce3a7e9e763854338964d9d69dc5a8a037
nathan-fiscaletti <nathan.fiscaletti@gmail.com>
2017-12-29 10:15:04 -0600

    SqlServers now connect when a query is made and persist after that. Creating default tables has been moved to the './synful -ct' command

--- src/Synful/Util/Functions/Sql.php
+++ src/Synful/Util/Functions/Sql.php
@@ -8,10 +8,50 @@ if (! function_exists('sf_sql')) {
      * @param string     $query
      * @param array      $binds
      * @param bool       $return
+     * @param string     $database
      * @return ResultSet
      */
-    function sf_sql($query, $binds = [], $return = false)
+    function sf_sql($query, $binds = [], $return = false, $database = 'main.synful')
     {
-        return \Synful\Synful::$sql->executeSql($query, $binds, $return);
+        if (\Synful\Synful::$sql_databases[$database] == null) {
+            if ($database == 'main.synful') {
+                trigger_error(
+                    'Primary database not configured. '
+                );
+            } else {
+                trigger_error(
+                    'Missing Synful database definition. '.
+                    'Set \'sqlservers.main.databases.synful\' in \'SqlServers.php\'. '.
+                    'Default Synful database is used for storing API Keys.',
+                    E_USER_WARNING
+                );
+                exit();
+            }
+            exit;
+        }
+
+        try {
+            return \Synful\Synful::$sql_databases[$database]->executeSql(
+                $query,
+                $binds,
+                $return
+            );
+        } catch (\Synful\Util\Framework\SynfulException $e) {
+            if ($database == 'main.synful') {
+                trigger_error(
+                    'Failed to connect to the primary Synful database. '.
+                    'Please check SqlServers.php. '.
+                    'Default Synful database is used for storing API Keys.',
+                    E_USER_WARNING
+                );
+                exit;
+            } else {
+                trigger_error(
+                    'Failed to connect to custom database \''.$database.'\'. Please check SqlServers.php.',
+                    E_USER_WARNING
+                );
+                exit;
+            }
+        }
     }
 }
