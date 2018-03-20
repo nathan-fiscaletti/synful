a62a66a85de9ef1d2c806eabadef7de89baf615e
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-03-20 14:17:11 -0500

    Added version command line command

---
+++ src/Synful/Util/CLIParser/Commands/Version.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Synful\Util\CLIParser\Commands;
+
+use Synful\Synful;
+use Synful\Util\CLIParser\Commands\Util\Command;
+
+class Version extends Command
+{
+    /**
+     * Construct the Version command.
+     */
+    public function __construct()
+    {
+        $this->name = 'v';
+        $this->description = 'Check the current version of the framework.';
+        $this->required = false;
+        $this->alias = 'version';
+
+        $this->exec = function () {
+            sf_info('Synful '.Synful::version());
+
+            exit;
+        };
+    }
+}
